b9fb4b505cd35c50dc72d16252af72f09ee4ce1e
Dustin <135719141+DustinTurska@users.noreply.github.com>
2023-09-07 00:24:13 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,18 @@ import type { AppProps } from "next/app";
 import { useState } from "react";
 import ChainContext from "../context/Chain";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Dogechain, Ethereum, Polygon, Mumbai, Goerli, Base } from "@thirdweb-dev/chains"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [selectedChain, setSelectedChain] = useState("ethereum");
 
   return (
     <ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
-      <ThirdwebProvider activeChain={selectedChain} clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENTID}>
+      <ThirdwebProvider 
+        activeChain={selectedChain}
+        supportedChains={[Dogechain, Ethereum, Polygon, Mumbai, Goerli, Base]}
+        clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENTID}
+      >
         <Component {...pageProps} />
       </ThirdwebProvider>
     </ChainContext.Provider>
