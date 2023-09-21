8dc8d40fc091853a3616e92fca7abb924b56f811
Dustin <135719141+DustinTurska@users.noreply.github.com>
2023-09-21 12:59:03 -0500

    Update SupportedChains.ts

--- context/SupportedChains.ts
+++ context/SupportedChains.ts
@@ -1,4 +1,4 @@
 
-import { Dogechain, Ethereum, Polygon, Mumbai, Goerli, Base, BaseGoerli, Binance, BinanceTestnet, Phoenix, Pulsechain, BitfinityNetworkTestnet, Alveychain, Avalanche, AvalancheFuji, Cronos, CronosTestnet } from "@thirdweb-dev/chains"
+import { ZksyncEraTestnet, Dogechain, Ethereum, Polygon, Mumbai, Goerli, Base, BaseGoerli, Binance, BinanceTestnet, Phoenix, Pulsechain, BitfinityNetworkTestnet, Alveychain, Avalanche, AvalancheFuji, Cronos, CronosTestnet } from "@thirdweb-dev/chains"
 
-export const supportedChains = [Dogechain, Ethereum, Polygon, Mumbai, Goerli, Base, BaseGoerli, Binance, BinanceTestnet, Phoenix, Pulsechain, BitfinityNetworkTestnet, Alveychain, Avalanche, AvalancheFuji, Cronos, CronosTestnet]
+export const supportedChains = [ZksyncEraTestnet, Dogechain, Ethereum, Polygon, Mumbai, Goerli, Base, BaseGoerli, Binance, BinanceTestnet, Phoenix, Pulsechain, BitfinityNetworkTestnet, Alveychain, Avalanche, AvalancheFuji, Cronos, CronosTestnet]
